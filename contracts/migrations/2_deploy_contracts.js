f36a7023c3214405ab3917d65b5a9ebae405c22a
Daniel Gretzke <daniel@gretzke.de>
2018-12-11 01:45:51 +0100

    async migration and setup

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,20 @@
 var Marketplace = artifacts.require('Marketplace.sol');
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
   // run if development network is used
   if ((network = 'development')) {
     const owner = accounts[0];
+    const admin1 = accounts[1];
+    const admin2 = accounts[2];
+    const storeOwner1 = accounts[3];
+    const storeOwner2 = accounts[4];
     // deploy marketplace contract from owner
-    deployer.deploy(Marketplace, { from: owner });
+    await deployer.deploy(Marketplace, { from: owner });
+
+    const mp = await Marketplace.deployed();
+    await mp.addAdmin(admin1, { from: owner });
+    await mp.addAdmin(admin2, { from: owner });
+    await mp.addStoreOwner(storeOwner1, { from: admin1 });
+    await mp.addStoreOwner(storeOwner2, { from: admin1 });
   }
 };
