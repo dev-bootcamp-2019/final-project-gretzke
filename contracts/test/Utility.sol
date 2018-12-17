bcea55c1685ca637b6ae9360ddd7827d55745379
Daniel Gretzke <daniel@gretzke.de>
2018-12-18 00:34:39 +0100

    added tests for adding items and stores

---
+++ contracts/test/Utility.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+/// @title utility library for truffle tests
+contract Utility {
+
+    function encodeID(string _name, uint256 _arrayLen, address _sender, uint256 _timestamp) public pure returns(bytes) {
+        return abi.encodePacked(_name, _arrayLen, _sender, _timestamp);
+    }
+
+}
--- contracts/test/test.js
+++ contracts/test/test.js
@@ -1,6 +1,8 @@
 const truffleAssert = require('truffle-assertions');
+const axios = require('axios');
 
-var Marketplace = artifacts.require('Marketplace.sol');
+const Marketplace = artifacts.require('Marketplace.sol');
+const Utility = artifacts.require('Utility.sol');
 
 contract('Marketplace', accounts => {
   before(async () => {
@@ -10,15 +12,16 @@ contract('Marketplace', accounts => {
     storeOwner1 = accounts[3];
     storeOwner2 = accounts[4];
     instance = await Marketplace.new({ from: owner });
+    utility = await Utility.new();
   });
 
   it('should check that owner was set correctly', async () => {
     // check if owner was set correctly
-    let testedOwner = await instance.owner.call();
+    const testedOwner = await instance.owner.call();
     assert.equal(owner, testedOwner, 'wrong owner');
   });
 
-  it('should add admins correctly', async () => {
+  it('should add admins', async () => {
     // try to add admin from a non-owner account, should fail
     await truffleAssert.fails(
       instance.addAdmin(admin1, { from: admin1 }),
@@ -28,34 +31,29 @@ contract('Marketplace', accounts => {
     );
 
     // add admins by owner
-    result = await instance.addAdmin(admin1, { from: owner });
+    let result = await instance.addAdmin(admin1, { from: owner });
     await instance.addAdmin(admin2, { from: owner });
-    // check if addedAdmin event was emitted
+    // check if AddedAdmin event was emitted
     truffleAssert.eventEmitted(
       result,
-      'addedAdmin',
+      'AddedAdmin',
       params => {
         return params.admin === admin1;
       },
-      'addedAdmin event should be emitted with admin as parameter'
+      'AddedAdmin event should be emitted with admin as parameter'
     );
     // event should not fire if addAdmin is called and admin is already added
     result = await instance.addAdmin(admin1, { from: owner });
-    truffleAssert.eventNotEmitted(
-      result,
-      'addedAdmin',
-      null,
-      'addedAdmin should not be emitted'
-    );
+    truffleAssert.eventNotEmitted(result, 'AddedAdmin', null, 'AddedAdmin should not be emitted');
 
     // check if admins were added
-    var isAdmin = await instance.admins.call(admin1);
+    let isAdmin = await instance.admins.call(admin1);
     assert.equal(isAdmin, true, 'admin1 should have been added');
     isAdmin = await instance.admins.call(admin2);
     assert.equal(isAdmin, true, 'admin2 should have been added');
   });
 
-  it('should add shopowners correctly', async () => {
+  it('should add shopowners', async () => {
     // try to add shopowner from a non-admin account, should fail
     await truffleAssert.fails(
       instance.addStoreOwner(storeOwner1, { from: storeOwner2 }),
@@ -65,35 +63,35 @@ contract('Marketplace', accounts => {
     );
 
     // add shopowners by admin
-    result = await instance.addStoreOwner(storeOwner1, { from: admin1 });
+    let result = await instance.addStoreOwner(storeOwner1, { from: admin1 });
     await instance.addStoreOwner(storeOwner2, { from: admin1 });
-    // check if addedStoreOwner event was emitted
+    // check if AddedStoreOwner event was emitted
     truffleAssert.eventEmitted(
       result,
-      'addedStoreOwner',
+      'AddedStoreOwner',
       params => {
         return params.storeOwner === storeOwner1 && params.admin === admin1;
       },
-      'addedStoreOwner event should be emitted with storeOwner1 and admin as parameter'
+      'AddedStoreOwner event should be emitted with storeOwner1 and admin as parameter'
     );
 
     // event should not fire if addStoreOwner is called and storeOwner is already added
     result = await instance.addStoreOwner(storeOwner1, { from: admin1 });
     truffleAssert.eventNotEmitted(
       result,
-      'addedStoreOwner',
+      'AddedStoreOwner',
       null,
-      'addedStoreOwner should not be emitted'
+      'AddedStoreOwner should not be emitted'
     );
 
     // check if shopowners were added
-    var isStoreOwner = await instance.storeOwners.call(storeOwner1);
+    let isStoreOwner = await instance.storeOwners.call(storeOwner1);
     assert.equal(isStoreOwner, true, 'shopowner1 should have been added');
     isStoreOwner = await instance.storeOwners.call(storeOwner2);
     assert.equal(isStoreOwner, true, 'shopowner2 should have been added');
   });
 
-  it('should remove admins correctly', async () => {
+  it('should remove admins', async () => {
     // try to remove admin from a non-owner account, should fail
     await truffleAssert.fails(
       instance.removeAdmin(admin2, { from: admin1 }),
@@ -101,31 +99,32 @@ contract('Marketplace', accounts => {
       'caller is not owner',
       'should have failed, should only be callable by owner'
     );
-    result = await instance.removeAdmin(admin2, { from: owner });
-    // check if removedAdmin event was emitted
+
+    let result = await instance.removeAdmin(admin2, { from: owner });
+    // check if RemovedAdmin event was emitted
     truffleAssert.eventEmitted(
       result,
-      'removedAdmin',
+      'RemovedAdmin',
       params => {
         return params.admin === admin2;
       },
-      'removedAdmin event should be emitted with admin as parameter'
+      'RemovedAdmin event should be emitted with admin as parameter'
     );
     // event should not fire if removeAdmin is called and admin is already removed
     result = await instance.removeAdmin(admin2, { from: owner });
     truffleAssert.eventNotEmitted(
       result,
-      'removedAdmin',
+      'RemovedAdmin',
       null,
-      'removedAdmin should not be emitted'
+      'RemovedAdmin should not be emitted'
     );
 
     // check if admin was removed
-    let isAdmin = await instance.admins.call(admin2);
+    const isAdmin = await instance.admins.call(admin2);
     assert.equal(isAdmin, false, 'admin2 should have been removed');
   });
 
-  it('should remove shopowners correctly', async () => {
+  it('should remove shopowners', async () => {
     // try to remove shopowner from a non-admin account, should fail
     await truffleAssert.fails(
       instance.removeStoreOwner(storeOwner2, { from: storeOwner1 }),
@@ -135,28 +134,148 @@ contract('Marketplace', accounts => {
     );
 
     // remove shopowners by admin
-    result = await instance.removeStoreOwner(storeOwner2, { from: admin1 });
-    // check if removedStoreOwner event was emitted
+    let result = await instance.removeStoreOwner(storeOwner2, { from: admin1 });
+    // check if RemovedStoreOwner event was emitted
     truffleAssert.eventEmitted(
       result,
-      'removedStoreOwner',
+      'RemovedStoreOwner',
       params => {
         return params.storeOwner === storeOwner2 && params.admin === admin1;
       },
-      'removedStoreOwner event should be emitted with storeOwner2 and admin as parameter'
+      'RemovedStoreOwner event should be emitted with storeOwner2 and admin as parameter'
     );
 
     // event should not fire if removeStoreOwner is called and storeOwner is already removed
     result = await instance.removeStoreOwner(storeOwner2, { from: admin1 });
     truffleAssert.eventNotEmitted(
       result,
-      'removedStoreOwner',
+      'RemovedStoreOwner',
       null,
-      'removedStoreOwner should not be emitted'
+      'RemovedStoreOwner should not be emitted'
     );
 
     // check if shopowner was removed
-    let isStoreOwner = await instance.storeOwners.call(storeOwner2);
+    const isStoreOwner = await instance.storeOwners.call(storeOwner2);
     assert.equal(isStoreOwner, false, 'shopowner2 should have been added');
   });
+
+  it('should add store', async () => {
+    const name = 'Test Name';
+    const description = 'this is a test description';
+    const sender = storeOwner1;
+
+    // try to add store from a non-storeowner account, should fail
+    await truffleAssert.fails(
+      instance.addStore(name, description, { from: admin1 }),
+      truffleAssert.ErrorType.REVERT,
+      'caller is not store owner',
+      'should have failed, should only be callable by store owners'
+    );
+
+    // retreive ID of first store from result
+    let result = await instance.addStore(name, description, { from: sender });
+    storeID1 = result.logs[0].args.storeID;
+
+    // add a second store
+    result = await instance.addStore(name, description, { from: sender });
+    // get timestamp of transaction to calculate the ID
+    const timestamp = await web3.eth.getBlock(result.receipt.blockNumber).timestamp;
+    const encodedPack = await utility.encodeID(name, 1, sender, timestamp);
+    storeID2 = web3.sha3(encodedPack, { encoding: 'hex' });
+
+    // calculated ID should match emitted ID in AddedStore event
+    await truffleAssert.eventEmitted(
+      result,
+      'AddedStore',
+      params => {
+        return params.storeOwner === sender && params.storeID === storeID2;
+      },
+      'AddedStore event should be emitted with storeOwner1 and ID as parameter'
+    );
+
+    store2 = await instance.getStore(sender, storeID2);
+    assert.equal(name, store2[0], 'name does not match');
+    assert.equal(description, store2[1], 'description does not match');
+    assert.equal(0, store2[2].length, 'item array should be empty');
+    assert.equal(1, store2[3], 'index should equal 1');
+    assert.equal(true, store2[4], 'active should be set to true');
+
+    // check if stores are in the correct order inside the store id list
+    storeIdList = await instance.getStoreIdList(sender);
+    assert.equal(2, storeIdList.length, 'Store ID list should have a length of 2');
+    assert.equal(storeID1, storeIdList[0], 'ID should match ID in store id list under first index');
+    assert.equal(
+      storeID2,
+      storeIdList[store2[3]],
+      'ID should match ID in store id list under store index'
+    );
+    // result = await instance.removeStore(ID, { from: sender });
+    // console.log(await instance.getStore(sender, ID));
+  });
+
+  it('should add item', async () => {
+    const name = 'Test Name';
+    const description = 'this is a test description';
+    const price = web3.toWei(0.01, 'ether');
+    const image = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    const stock = 1;
+    const sender = storeOwner1;
+
+    // try to add item from a non-storeowner account, should fail
+    await truffleAssert.fails(
+      instance.addItem(storeID2, name, description, price, image, stock, {
+        from: admin1
+      }),
+      truffleAssert.ErrorType.REVERT,
+      'caller is not store owner',
+      'should have failed, should only be callable by store owners'
+    );
+
+    // retreive ID of first item from result
+    let result = await instance.addItem(storeID2, name, description, price, image, stock, {
+      from: sender
+    });
+    itemID1 = result.logs[0].args.itemID;
+    // add a second item
+    result = await instance.addItem(storeID2, name, description, price, image, stock, {
+      from: sender
+    });
+    // get timestamp of transaction to calculate the ID
+    const timestamp = await web3.eth.getBlock(result.receipt.blockNumber).timestamp;
+    const encodedPack = await utility.encodeID(name, 1, sender, timestamp);
+    itemID2 = web3.sha3(encodedPack, { encoding: 'hex' });
+
+    // calculated ID should match emitted ID in AddedItem event
+    await truffleAssert.eventEmitted(
+      result,
+      'AddedItem',
+      params => {
+        return (
+          params.storeOwner === sender && params.storeID === storeID2 && params.itemID === itemID2
+        );
+      },
+      'AddedItem event should be emitted with storeOwner1, storeID and itemID as parameter'
+    );
+
+    item2 = await instance.getItem(sender, storeID2, itemID2);
+    assert.equal(name, item2[0], 'name does not match');
+    assert.equal(description, item2[1], 'description does not match');
+    assert.equal(image, item2[2], 'image bytes do not match');
+    assert.equal(price, item2[3], 'price does not match');
+    assert.equal(stock, item2[4], 'stock does not match');
+    assert.equal(1, item2[5], 'index should equal 1');
+    assert.equal(true, item2[6], 'active should be set to true');
+
+    // check if items are in the correct order inside the store id list
+    itemIdList = await instance.getItemIdList(sender, storeID2);
+    assert.equal(2, itemIdList.length, 'Item ID list should have a length of 2');
+    assert.equal(itemID1, itemIdList[0], 'ID should match ID in item id list under first index');
+    assert.equal(
+      itemID2,
+      itemIdList[item2[5]],
+      'ID should match ID in item id list under item index'
+    );
+  });
+  it('should remove item', async () => {});
+  it('should remove store', async () => {});
 });
